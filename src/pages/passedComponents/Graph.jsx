aa1a154356440fe98a997aaa54e2300c05f60d72
Demostat101 <demostat1995@gmail.com>
2024-07-10 19:12:35 +0100

    modified

--- src/pages/passedComponents/Graph.jsx
+++ src/pages/passedComponents/Graph.jsx
@@ -1,4 +1,4 @@
-"use client";
+
 import { BarChart, Bar,ResponsiveContainer, XAxis, YAxis, Tooltip, Legend } from "recharts";
 import { salesData } from "./GraphData";
 const halfsales = salesData.slice(0,6);
@@ -8,8 +8,7 @@ console.log(halfsales);
 const Graph = () => {
   return (
        <ResponsiveContainer width="100%" height="100%">
-             <BarChart width={500} height={400} data={halfsales}>
-             {/* <CartesianGrid strokeDasharray="3 3"  /> */}
+             <BarChart data={halfsales}>
              <XAxis dataKey="month"/>
              <YAxis/>
              <Tooltip/>
--- src/pages/passedComponents/MiddleComponent.jsx
+++ src/pages/passedComponents/MiddleComponent.jsx
@@ -6,8 +6,32 @@ const MiddleComponent = () => {
 
     const {open} = Context()
   return (
-    <main className={open ? "2xl:w-[1127px] 2xl:h-[295.71px] flex 2xl:gap-[30px] md:w-[100%] md:h-[100%] border-solid border-2 border-red-500" : "2xl:w-[1240.9px] 2xl:h-[325.59px] flex 2xl:gap-[33.03px] md:w-[100%] md:h-[100%] border-solid border-2 border-yellow-500"}>
-        <Graph/>
+    <main className={open ? "2xl:w-[1127px] 2xl:h-[295.71px] flex 2xl:gap-[30px] md:w-[100%] md:h-[250px] border-solid border-2 border-red-500" : "2xl:w-[1240.9px] 2xl:h-[325.59px] flex 2xl:gap-[33.03px] md:w-[100%] md:h-[280px] border-solid border-2 border-yellow-500"}>
+
+      {/* Graph container / right side container */}
+        <div className={open ? "2xl:w-[739px] 2xl:h-[295px] border-solid border-2 border-green-600 md:w-[500px] md:h-[250px]" : "2xl:w-[813.69px] 2xl:h-[324.81px] md:w-[550px] md:h-[330px]"} >
+          {/* Top component inside graph with drop downs */}
+          <div className={open ? "2xl:w-[698px] 2xl:h-[42px] 2xl:gap-[154px] flex md:w-[100%] md:h-[30px] md:justify-between border-solid border-2 border-green-600" : "2xl:w-[768.67px] 2xl:h-[45.82px] 2xl:gap-[169.56px] flex md:w-[95%] md:h-[30px] md:justify-between"}>
+            {/* graph top content */}
+            <div className={open ? "2xl:w-[256px] 2xl:h-[24px] 2xl:text-[16px] 2xl:leading-[24px] font-[500] md:w-[20%] md:h-[100%] md:text-[12px] md:leading-[18px] md:text-nowrap" : "2xl:w-[282px] 2xl:h-[26px] 2xl:text-[17.62px] 2xl:leading-[26.43px] font-[500] md:w-[30%] md:h-[100%] md:text-nowrap md:text-[14px] md:leading-[20px]"} >Employee Availability Overview</div>
+
+            <div className={open ? "2xl:w-[288px] 2xl:h-[42px] 2xl:gap-[10px] md:w-[200px] md:h-[100%] md:gap-[5px]" : "2xl:w-[317.11px] 2xl:h-[45.82px] 2xl:gap-[11.01px] md:w-[250px] md:h-[100%] md:gap-[5px]"} >
+              {/* left drop down */}
+              <div className={open ? "2xl:w-[108px] 2xl:h-[42px] flex flex-col 2xl:gap-[10px] md:w-[100px] md:h-[100%] " : "2xl:w-[118.91px] 2xl:h-[45.82px] flex flex-col 2xl:gap-[11.01px] md:w-[100px] md:h-[100%] "} >
+                {/* left drop down select */}
+
+                <select name="" id="">
+                  <option value="Sale">Sale</option>
+                  <option value="Nosale">No Sales</option>
+                </select>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+        {/* <Graph/> */}
       
     </main>
   )
