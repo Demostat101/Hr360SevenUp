6b14c25dc8f255b0e96e3816a7e4391309434ea0
Demostat101 <demostat1995@gmail.com>
2024-07-11 18:52:52 +0100

    modified

--- src/pages/dashBoardComponents/bottomComponents/Box3.jsx
+++ src/pages/dashBoardComponents/bottomComponents/Box3.jsx
@@ -19,7 +19,7 @@ const Box3 = () => {
         {/* top right bar */}
 
         <div className={open ? "2xl:w-[128px] 2xl:h-[31px] md:w-[120px] md:h-[25px] " : "2xl:w-[140.94px] 2xl:h-[34.13px] md:w-[125px] md:h-[30px] "}>
-          <select name="" id="">
+          <select className=" outline-none" name="" id="">
             <option value="">This week</option>
             <option value="">Last week</option>
           </select>
@@ -29,10 +29,12 @@ const Box3 = () => {
 
       </div>
 
-      <div className={open ? "2xl:w-[100%] 2xl:h-[21px] flex place-items-center justify-between  md:w-[270px] md:h-[18px]  " :  "2xl:w-[100%] 2xl:h-[23px] justify-between flex place-items-center  md:w-[300px] md:h-[20px] "}>
-        <div className={open ? "2xl:w-[67px] 2xl:h-[21px] birth 2xl:font-[400] 2xl:text-[14px] 2xl:leading-[21px] md:w-[60px] md:h-[18px] md:font-[400] md:text-[10px] md:leading-[18px] " : "2xl:w-[74px] 2xl:h-[23px] 2xl:font-[400] 2xl:text-[15.41px] 2xl:leading-[23.12px] md:w-[70px] md:h-[20px] md:font-[400] md:text-[12px] md:leading-[20px] "}>Birthday</div>
+      <div className={open ? "2xl:w-[100%]  2xl:h-[21px] gap-[30px] flex place-items-center justify-center  md:w-[100%] md:h-[18px]  " :  "2xl:w-[100%] pt-[20px]  2xl:h-[23px] justify-center flex place-items-center  md:w-[100%] md:h-[20px] "}>
+       <div className="w-full flex justify-between pl-[50px]">
+       <div className={open ? "2xl:w-[67px] 2xl:h-[21px] birth 2xl:font-[400] 2xl:text-[14px] 2xl:leading-[21px] md:w-[60px] md:h-[18px] md:font-[400] md:text-[10px] md:leading-[18px] " : "2xl:w-[74px] birth 2xl:h-[23px] 2xl:font-[400] 2xl:text-[15.41px] 2xl:leading-[23.12px] md:w-[70px] md:h-[20px] md:font-[400] md:text-[12px] md:leading-[20px] "}>Birthday</div>
 
-        <div className={ open ? "2xl:w-[136px] 2xl:h-[21px] 2xl:font-[400] 2xl:text-[14px] 2xl:leading-[21px] md:w-[130px] md:h-[18px] md:font-[400] md:text-[10px] md:leading-[18px] " : "2xl:w-[149px] 2xl:h-[23px] 2xl:font-[400] 2xl:text-[15.41px] 2xl:leading-[23.12px] md:w-[140px] md:h-[20px] md:font-[400] md:text-[12px] md:leading-[20px] "}>Anniversary</div>
+<div className={ open ? "2xl:w-[136px] 2xl:h-[21px] 2xl:font-[400] 2xl:text-[14px] 2xl:leading-[21px] md:w-[130px] md:h-[18px] md:font-[400] md:text-[10px] md:leading-[18px] " : "2xl:w-[149px] 2xl:h-[23px] 2xl:font-[400] 2xl:text-[15.41px] 2xl:leading-[23.12px] md:w-[140px] md:h-[20px] md:font-[400] md:text-[12px] md:leading-[20px] "}>Anniversary</div>
+       </div>
       </div>
 
       
