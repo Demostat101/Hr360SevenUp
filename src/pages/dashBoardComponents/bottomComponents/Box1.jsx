50c1fb86be2adc2d67752ccfa539f7acd9a22c81
Demostat101 <demostat1995@gmail.com>
2024-07-11 14:18:38 +0100

    modified

--- src/pages/dashBoardComponents/bottomComponents/Box1.jsx
+++ src/pages/dashBoardComponents/bottomComponents/Box1.jsx
@@ -1,4 +1,5 @@
 import { Context } from "../../../DashBoardContext";
+import { Box1Data } from "./Box1Array";
 
 const Box1 = () => {
   const { open } = Context();
@@ -7,8 +8,8 @@ const Box1 = () => {
     <div
       className={
         open
-          ? "2xl:w-[349.55px] bg-[#FFFFFF] rounded-lg 2xl:h-[311.1px] md:w-[320px] md:h-[300px] shadow-lg "
-          : "2xl:w-[384.88px] bg-[#FFFFFF] 2xl:h-[342.55px]  rounded-lg md:w-[340px] md:h-[300px] shadow-lg"
+          ? "2xl:w-[349.55px] bg-[#FFFFFF] flex flex-col gap-[30px] rounded-lg 2xl:h-[311.1px] md:w-[320px] md:h-[300px] shadow-lg "
+          : "2xl:w-[384.88px] bg-[#FFFFFF] 2xl:h-[342.55px] flex flex-col gap-[30px] rounded-lg md:w-[340px] md:h-[300px] shadow-lg"
       }
     >
       {/* top box */}
@@ -23,54 +24,65 @@ const Box1 = () => {
       </div>
 
       {/* Boxes */}
-      <div>
+      <div
+        className={
+          open
+            ? "flex flex-col gap-[20px] place-items-center "
+            : "flex flex-col gap-[20px] place-items-center"
+        }
+      >
         {/* 1st box */}
-        <div
-          className={
-            open
-              ? " 2xl:w-[309.36px] 2xl:h-[56.51px] flex md:w-[80%] md:h-[50px] border-[1.17px] border-[#ECEEF6]"
-              : " 2xl:w-[340.62px] 2xl:h-[62.22px] flex md:w-[80%] md:h-[55px] border-[1.17px] border-[#ECEEF6]"
-          }
-        >
-          {/* small box */}
-          <div
-            className={
-              open
-                ? " 2xl:w-[28px] 2xl:h-[42px] md:w-[20px] md:h-[100%] "
-                : " 2xl:w-[31px] 2xl:h-[46px] md:w-[23px] md:h-[100%] "
-            }
-          >
-            <span>18</span> <span>mar</span>{" "}
-          </div>
+        {Box1Data.map(({ date, time, title, month }, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                open
+                  ? " 2xl:w-[309.36px] 2xl:h-[56.51px] border-l-[5px] border-l-[#176B87] place-items-center rounded-md flex gap-[40px] md:w-[80%] md:h-[50px] border-[1.17px] border-[#ECEEF6]"
+                  : " 2xl:w-[340.62px] 2xl:h-[62.22px] flex md:w-[80%] border-l-[5px] place-items-center border-l-[#176B87] rounded gap-[40px] md:h-[55px] border-[1.17px] border-[#ECEEF6]"
+              }
+            >
+              {/* small box */}
+              <div
+                className={
+                  open
+                    ? " 2xl:w-[28px] 2xl:h-[42px] md:w-[20px] pl-[30px] flex flex-col justify-center text-center place-items-center md:h-[100%] "
+                    : " 2xl:w-[31px] 2xl:h-[46px] md:w-[23px] pl-[30px] flex flex-col justify-center text-center md:h-[100%] place-items-center"
+                }
+              >
+                <span>{date}</span> <span>{month}</span>{" "}
+              </div>
 
-          {/* 2nd Box */}
-          <div
-            className={
-              open
-                ? " 2xl:w-[113px] 2xl:h-[39px] md:w-[100px] md:h-[100%]"
-                : " 2xl:w-[124px] 2xl:h-[43px] md:w-[110px] md:h-[100%]"
-            }
-          >
-            <span
-              className={
-                open
-                  ? " 2xl:text-[14px] 2xl:leading-[21px] 2xl:font-[500] md:text-[10px] md:leading-[15px] md:font-[500] "
-                  : " 2xl:text-[15.4px] 2xl:leading-[23.12px] 2xl:font-[500] md:text-[13px] md:leading-[17px] md:font-[500] "
-              }
-            >
-              Sabi Cafe Event
-            </span>{" "}
-            <span
-              className={
-                open
-                  ? " 2xl:text-[12px] 2xl:leading-[18px] 2xl:font-[400] md:text-[8px] md:leading-[12px] md:font-[400] "
-                  : " 2xl:text-[13.21px] 2xl:leading-[19.82px] 2xl:font-[400] md:text-[10px] md:leading-[15px] md:font-[400] "
-              }
-            >
-              2:00pm
-            </span>
-          </div>
-        </div>
+              {/* 2nd Box */}
+              <div
+                className={
+                  open
+                    ? " 2xl:w-[113px] 2xl:h-[39px] md:w-[100px] flex flex-col justify-evenly md:h-[100%]"
+                    : " 2xl:w-[124px] 2xl:h-[43px] md:w-[110px] flex flex-col justify-evenly md:h-[100%]"
+                }
+              >
+                <span
+                  className={
+                    open
+                      ? " 2xl:text-[14px] 2xl:leading-[21px] text-nowrap 2xl:font-[500] md:text-[10px] md:leading-[15px] md:font-[500] "
+                      : " 2xl:text-[15.4px] 2xl:leading-[23.12px] text-nowrap 2xl:font-[500] md:text-[13px] md:leading-[17px] md:font-[500] "
+                  }
+                >
+                  {title}
+                </span>{" "}
+                <span
+                  className={
+                    open
+                      ? " 2xl:text-[12px] 2xl:leading-[18px] 2xl:font-[400] md:text-[8px] md:leading-[12px] md:font-[400] "
+                      : " 2xl:text-[13.21px] 2xl:leading-[19.82px] 2xl:font-[400] md:text-[10px] md:leading-[15px] md:font-[400] "
+                  }
+                >
+                  {time}
+                </span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/pages/dashBoardComponents/bottomComponents/Box2.jsx
+++ src/pages/dashBoardComponents/bottomComponents/Box2.jsx
@@ -6,7 +6,13 @@ const Box2 = () => {
 
     const {open} = Context()
   return (
-    <div className={open ? "2xl:w-[349.55px] 2xl:h-[311.1px] border-solid border-2 border-red-800" :"2xl:w-[384.88px] 2xl:h-[342.55px] border-solid border-2 border-blue-400"}>
+    <div className={open ? "2xl:w-[349.55px] 2xl:h-[311.1px] bg-[#FFFFFF] rounded-lg shadow-lg md:w-[300px] md:h-[300]" :"2xl:w-[384.88px] rounded-lg shadow-lg 2xl:h-[342.55px] bg-[#FFFFFF] md:w-[310px] md:h-[305]"}>
+
+      {/* top container */}
+      <div className={open ? "2xl:w-[349px] border-[1.17px]  border-[#ECEEF6] 2xl:h-[55px] flex place-items-center justify-between 2xl:gap-[10px] pl-[20px] pr-[20px] md:w-[100%] md:h-[35px] md:gap-[7px]" : "2xl:w-[384.53px] 2xl:h-[60.44px] flex place-items-center justify-between 2xl:gap-[11.01px] pr-[20px] pl-[20px] md:w-[100%] border-[1.18px] border-[#ECEEF6] md:h-[38px] md:gap-[8px] "} >
+        <h2 className={open ? " 2xl:w-[136px]  2xl:h-[24px] font-[500] 2xl:text-[16px] 2xl:leading-[24px] md:w-[130px] md:h-[20px] md:text-[10px] md:leading-[18px] " : " 2xl:w-[150px] 2xl:h-[26px] font-[500] 2xl:text-[17.62px] 2xl:leading-[26.43px] md:w-[120px] md:h-[22px] md:text-[12px] md:leading-[20] "} >Announcement</h2>
+        <button className={open ? "2xl:w-[110px] 2xl:h-[31px] rounded-lg border-[#E3EFF3] border-solid border-2  md:w-[90px] md:h-[26] " : "2xl:w-[121.12px] 2xl:h-[34.01px] rounded-lg md:w-[100px] border-solid border-[#E3EFF3] border-[2px] md:h-[28] "}>Create</button>
+      </div>
 
       
     </div>
